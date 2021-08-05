19f7d998ef074b1cc84aa5d532ec431151841951
ankit <ac2000choudhary@gmail.com>
2021-08-05 13:18:00 +0530

    main page updated

--- src/App.js
+++ src/App.js
@@ -1,24 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from './Components/Header.js';
+import Todos from './Components/Todos.js';
+import {Footer} from './Components/Footer.js';
+import React, { useState, useEffect } from 'react';
+import { AddTodo } from './Components/AddTodo.js';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import { About } from './Components/About.js'
+
 
 function App() {
+  let initTodos;
+
+  if(localStorage.getItem('todos') === null) {
+    initTodos = [];
+  }
+  else {
+    initTodos = JSON.parse(localStorage.getItem('todos'));
+  }
+
+  const [todos, setTodos] = useState(initTodos);
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos] );
+
+  const addTodo = (title, desc) => {
+    let sno=0;
+
+    if(todos.length!==0){
+      sno = todos[todos.length - 1].sno + 1;
+    }
+
+    const myTodo = {
+      sno: sno,
+      title: title,
+      desc: desc
+    }
+    setTodos([...todos, myTodo]);
+
+  }
+
+  const onDelete = (todo) => {
+    setTodos(todos.filter((e)=>{
+      return e!==todo;
+    }));
+  };
+
+  const burger = (e) => {
+    // console.log(e.target.dataset.target);
+    var nav = document.querySelector('#'+e.target.dataset.target);
+    nav.classList.toggle('is-active');
+    e.target.classList.toggle('is-active');
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Header title="My TodosList" burger={burger}/>
+      <Switch>
+        <Route exact path="/" render = {() => { 
+          return(
+            <>
+            <AddTodo addTodo={addTodo}/>
+            <Todos todo={todos} onDelete={onDelete}/>
+            </>
+          )}}>
+        </Route>
+        <Route exact path="/about">
+          <About/>
+        </Route>
+      </Switch>
+      <Footer/>
+    </Router>
   );
 }
 
