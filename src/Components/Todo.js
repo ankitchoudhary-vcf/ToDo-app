4f60c9bc7e1e2901ac4d257e4571a3640cf2d28c
ankit <ac2000choudhary@gmail.com>
2021-08-07 09:34:22 +0530

    Todo updated

--- src/Components/Todo.js
+++ src/Components/Todo.js
@@ -1,11 +1,22 @@
-import React from 'react'
+import React from "react";
 
 export default function Todo(props) {
-    return (
-        <>
-            <h4 className="title is-4 mb-0">{props.todo.title}</h4>
-            <p className="content m-2 pt-0">{props.todo.desc}</p>
-            <button className="button is-danger" onClick={() => {props.onDelete(props.todo)}}>Delete</button>
-        </>
-    )
+  return (
+    <>
+      <div className="column notification is-6-desktop is-size-3-desktop is-size-5-touch is-box m-2">
+        <h4 className="title is-size-3-desktop is-size-5-touch mb-0">
+          {props.todo.title}
+        </h4>
+        <p className="content my-2 pt-0">{props.todo.desc}</p>
+        <button
+          className="button is-danger is-pulled-right"
+          onClick={() => {
+            props.onDelete(props.todo);
+          }}
+        >
+          Delete
+        </button>
+      </div>
+    </>
+  );
 }
