3f91d2053f039d7c1c82549ad390e9167d1dc45b
ankit <ac2000choudhary@gmail.com>
2021-08-07 09:34:42 +0530

    Footer updated

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,13 +1,18 @@
-import React from 'react'
+import React from "react";
 
 export const Footer = () => {
-    let footerStyle = {
-        position: 'relative',
-        top: '40vh'
-    }
-    return (
-        <div className="notification is-dark footer has-text-centered" style={footerStyle}>
-            Todos List Created by Ankit Choudhary
-        </div>
-    )
-}
+  let footerStyle = {
+    position: "relative",
+    top: "40vh",
+  };
+  return (
+    <footer className="footer has-background-dark" style={footerStyle}>
+      <div class="content has-text-centered has-text-white">
+        <p>
+          <strong className="has-text-white is-family-code">Todos List</strong> Created by{" "}
+          <a href="https://github.com/ankit1509">Ankit Choudhary</a>.
+        </p>
+      </div>
+    </footer>
+  );
+};
