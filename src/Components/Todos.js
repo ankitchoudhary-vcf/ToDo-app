e85ae08d8c08c7ee9f09f6c505d3f64d38c13e04
ankit <ac2000choudhary@gmail.com>
2021-08-07 09:34:13 +0530

    Todos updated

--- src/Components/Todos.js
+++ src/Components/Todos.js
@@ -4,22 +4,21 @@ import Todo from '../Components/Todo.js'
 export default function Todos(props) {
     return (
         <div className="container">
-            <h3 className="title is-3 notification is-box is-primary  has-text-centered">Todos List</h3>
+            <h3 className="title is-size-3-desktop is-size-5-touch notification is-box is-primary  has-text-centered">Todos List</h3>
             <hr style={{backgroundColor: '#00d1b2'}} />
             <hr className="mb-6" style={{backgroundColor: '#00d1b2'}} />
             <div className="columns is-centered is-mobile is-multiline">
 
                 {
-                    props.todo.length === 0 ? <h3 className="notification is-box is-warning is-light subtitle is-4">No Todos to display</h3> :
+                    props.todo.length === 0 ? <h3 className="notification is-box is-warning is-light mb-3 is-size-3-desktop is-size-5-touch m-1 p-3">No Todos to display</h3> :
                         props.todo.map((todo) => {
                             return (
-                                <div className="column notification is-box m-2" key={todo.sno}>
-                                    <Todo todo={todo} onDelete={props.onDelete} />
-                                </div>
+                                        <Todo key={todo.sno} todo={todo} onDelete={props.onDelete} />
                             )
                         })
                 }
             </div>
+      <hr style={{backgroundColor: '#00d1b2'}} />
         </div>
     )
 }
